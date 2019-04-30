7ccb1a700bdc9774ccb283c6f8c13e5181eaf8cf
liwang <liwang112358@gmail.com>
2019-04-30 11:45:24 +0800

    network

---
+++ c/NetWork.cpp
@@ -0,0 +1,16 @@
+#include "Network.h"
+
+
+
+NetWork::NetWork(const std::vector<int> &sizes)
+    : num_layers(sizes.size())
+    
+{
+
+    
+
+}
+
+NetWork::~NetWork()
+{
+}
---
+++ c/NetWork.h
@@ -0,0 +1,17 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <vector>
+
+class NetWork
+{
+private:
+    
+public:
+    NetWork::NetWork(const std::vector<int> sizes);
+    ~NetWork();
+
+};
+
+
+#endif
