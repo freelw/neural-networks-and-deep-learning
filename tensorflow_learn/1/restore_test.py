265009885dda438a93c558f3093220b6e73b5029
wangli <liwang112358@gmail.com>
2019-05-15 17:57:53 +0800

    inference test

--- tensorflow_learn/1/restore_test.py
+++ tensorflow_learn/1/restore_test.py
@@ -1,11 +1,18 @@
 #coding: utf-8
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_DATA/", one_hot=True)
 W = tf.Variable(tf.zeros([784,10]), name='W')
 b = tf.Variable(tf.zeros([10]), name='b')
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
-
 with tf.Session() as sess:
     saver.restore(sess, "save/model.ckpt")
     print tf.all_variables()
     print 'W: ', sess.run(W)
     print 'b: ', sess.run(b)
+    print (sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
