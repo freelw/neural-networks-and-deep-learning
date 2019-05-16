999a2815a5bb3b64aadda0e908ff2749b4eec8de
wangli <liwang112358@gmail.com>
2019-05-16 14:37:24 +0800

    write import graph succ

---
+++ tensorflow_learn/1/restore_from_graphpb_test.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_DATA/", one_hot=True)
+x = tf.placeholder(tf.float32, [None, 784], name='input_x')
+y_ = tf.placeholder(tf.float32, [None, 10], name='input_y')
+with open('./model/restore_test.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    print graph_def
+    [accuracy] = tf.import_graph_def(graph_def, input_map={'input_x': x, 'input_y': y_}, return_elements=['accuracy:0'])
+with tf.Session() as sess:
+    print sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels})
--- tensorflow_learn/1/softmax_regression.py
+++ tensorflow_learn/1/softmax_regression.py
@@ -2,29 +2,21 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_DATA/", one_hot=True)
-
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.placeholder(tf.float32, [None, 784], name='input_x')
 W = tf.Variable(tf.zeros([784,10]), name='W')
 b = tf.Variable(tf.zeros([10]), name='b')
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder(tf.float32, [None, 10])
-
+y = tf.nn.softmax(tf.matmul(x, W) + b, name='output_y')
+y_ = tf.placeholder(tf.float32, [None, 10], name='input_y')
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y)))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
 sess = tf.InteractiveSession()
-
 tf.global_variables_initializer().run()
 for _ in xrange(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1), name='correct')
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+output_graph_def = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def, output_node_names=['accuracy'])
+with tf.gfile.FastGFile('model/restore_test.pb', mode='wb') as f:
+        f.write(output_graph_def.SerializeToString())
 print (sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
-
-saver = tf.train.Saver()
-save_path = saver.save(sess, "save/model.ckpt")
-print tf.all_variables()
-print 'W: ', sess.run(W)
-print 'b: ', sess.run(b)
