a128f2355997d9d18ef15091de88e1f251ca61ef
李望 <dawang@leadin.cloud>
2025-01-06 05:09:08 +0000

    on mac

---
+++ init_py_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#pip install -r requirements.txt -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip install -r requirements.txt -i https://mirrors.cloud.tencent.com/pypi/simple
